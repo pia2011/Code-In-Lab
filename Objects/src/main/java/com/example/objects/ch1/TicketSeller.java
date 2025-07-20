433ce58b64d29f176442080b25af4c1a66789aec
 pia2011 <pia2011@mailplug.co.kr>
2025-07-21 07:46:05 +0900

    [오브젝트] 판매원 캡슐화

--- Objects/src/main/java/com/example/objects/ch1/TicketSeller.java
+++ Objects/src/main/java/com/example/objects/ch1/TicketSeller.java
@@ -6,8 +6,17 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
-
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            // 초대장이 있는 경우
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            // 초대장이 없는 경우
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
